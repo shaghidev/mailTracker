dae94445a715a17e68ac8abc33f85bef80ddce88
OGADAMo <adam.marjanovic2@gmail.com>
2025-09-11 21:41:17 +0200

    log fix

--- tracker.py
+++ tracker.py
@@ -1,27 +1,30 @@
 import os
 from flask import Flask, request, redirect
+import logging
 
 app = Flask(__name__)
+
+# Isključi Flask default request log (werkzeug logger)
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 @app.route("/")
 def home():
     return "Flask app radi! 🚀"
 
-# Open tracking
 @app.route("/track_open")
 def track_open():
     email = request.args.get("email")
     print(f"[OPEN] {email}")
-    return "", 200  # transparentni pixel bi tu mogao ići, ali za test dosta je 200
+    return "", 200
 
-# Click tracking
 @app.route("/track_click")
 def track_click():
     email = request.args.get("email")
-    link = request.args.get("link", "https://baltazargrad.com")  # fallback link
+    link = request.args.get("link", "https://baltazargrad.com")
     print(f"[CLICK] {email}")
-    return redirect(link, code=302)  # normalno redirekta korisnika
+    return redirect(link, code=302)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # koristi PORT iz okoline
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
