e45109fb7501843b194a07ae337722fe39af8dde
OGADAMo <adam.marjanovic2@gmail.com>
2025-09-12 12:09:14 +0200

    cors policy

--- server/tracker.py
+++ server/tracker.py
@@ -3,8 +3,10 @@ from datetime import datetime
 from bson import ObjectId
 from config import campaigns_collection, mails_collection, events_collection
 import io
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Omogućava sve domene
 
 # --- Kreiranje nove kampanje ---
 @app.route("/create_campaign", methods=["POST"])
