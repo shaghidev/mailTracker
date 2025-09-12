76f7a97df21ad88005683167a18792eb0ce383bf
OGADAMo <adam.marjanovic2@gmail.com>
2025-09-12 09:24:55 +0200

    dodavanje kolekcija

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ from pymongo import MongoClient
 import os
 
 client = MongoClient(os.environ["MONGO_URI"])
-db = client.get_database("mailTracker")  # ime baze mora biti ovdje
-events_collection = db.events
+db = client.get_database("mailTracker")  # ime baze
+
+events_collection = db.events        # logovi: sent, open, click
+mails_collection  = db.mails         # poslane email adrese, statusi itd.
