e60581a6c79565967b5292fbabbf297acc32c52e
OGADAMo <adam.marjanovic2@gmail.com>
2025-09-15 08:46:48 +0200

    login

--- server/routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers.auth_controller import register, login
-
-auth_bp = Blueprint("auth", __name__)
-
-auth_bp.route("/register", methods=["POST"])(register)
-auth_bp.route("/login", methods=["POST"])(login)
--- server/routes/tracking_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from controllers.tracking_controller import track_open, track_click, track_sent
-
-tracking_bp = Blueprint("tracking", __name__)
-
-tracking_bp.route("/track_open")(track_open)
-tracking_bp.route("/track_click")(track_click)
-tracking_bp.route("/track_sent")(track_sent)
