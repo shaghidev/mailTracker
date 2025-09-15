9f738e370ab2323ee041b6b9f320a2beafbe414c
shaghidev <adam.marjanovic2@gmail.com>
2025-09-15 12:07:20 +0200

    cors problem

--- server/app.py
+++ server/app.py
@@ -1,20 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Dopuštamo sve metode i HEAD, OPTIONS
+# Omogućavamo CORS za sve domene, sve metode i sve header-e
 CORS(
     app,
-    origins=["http://localhost:3000", "https://mailtracker-7jvy.onrender.com"],
+    origins="*",  # dopušta sve domene
     supports_credentials=True,
-    methods=["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"]
+    methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["*"]  # dopušta sve header-e, uključujući Authorization
 )
 
-# --- tvoji blueprinti ---
+# Rješavamo OPTIONS preflight zahtjeve
+@app.before_request
+def handle_options():
+    if request.method == "OPTIONS":
+        return "", 200
+
+# --- registracija blueprinta ---
 from routes import campaigns, mails, contacts
-app.register_blueprint(campaigns.bp, url_prefix="/")
+
+app.register_blueprint(campaigns.bp, url_prefix="/api/campaigns")
 app.register_blueprint(mails.bp, url_prefix="/api/mails")
 app.register_blueprint(contacts.bp, url_prefix="/api/contact_lists")
 
