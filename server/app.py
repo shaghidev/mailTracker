40b446b3c7594ed5d0e365e1af15031b35139f66
shaghidev <adam.marjanovic2@gmail.com>
2025-09-15 11:44:01 +0200

    cors

--- server/app.py
+++ server/app.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import campaigns, mails, contacts
 
 app = Flask(__name__)
 
-# Ovo dopušta sve domene, sve metode i Authorization header
+# Dopuštamo sve metode i HEAD, OPTIONS
 CORS(
     app,
-    origins="*",  # sve domene
+    origins=["http://localhost:3000", "https://mailtracker-7jvy.onrender.com"],
     supports_credentials=True,
-    methods=["GET","POST","DELETE","PUT","PATCH","OPTIONS"],
+    methods=["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization"]
 )
 
+# --- tvoji blueprinti ---
+from routes import campaigns, mails, contacts
 app.register_blueprint(campaigns.bp, url_prefix="/api/campaigns")
 app.register_blueprint(mails.bp, url_prefix="/api/mails")
 app.register_blueprint(contacts.bp, url_prefix="/api/contact_lists")
